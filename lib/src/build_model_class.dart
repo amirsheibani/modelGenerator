febc5d06228f1082afa4c56f80d2de73b23d944a
a.sheibani <sheibani.amir@gmail.com>
2022-08-21 18:09:19 +0430

    add annotations dependencies

--- lib/src/build_model_class.dart
+++ lib/src/build_model_class.dart
@@ -1,9 +1,9 @@
 
+import 'package:annotations/generate_model_annotations.dart';
 import 'package:build/src/builder/build_step.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'package:annotations/annotations.dart';
 
 import 'model_visitor.dart';
 
--- lib/src/build_model_mapper.dart
+++ lib/src/build_model_mapper.dart
@@ -1,5 +1,5 @@
 
-import 'package:annotations/annotations.dart';
+import 'package:annotations/generate_model_annotations.dart';
 import 'package:build/src/builder/build_step.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:source_gen/source_gen.dart';
